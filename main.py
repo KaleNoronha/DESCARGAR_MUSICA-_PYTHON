157d8558e7f63000ca214114d9974b826be6cf2c
KaleNoronha <kaleNoronha@outlook.com.pe>
2024-12-30 11:15:45 -0500

    primer commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import yt_dlp
+import os
+def descargar_musica (url,carpeta="MUSICA"):
+    try:
+        if not os.path.exists(carpeta):
+            os.makedirs(carpeta)
+        opciones = {
+            'format': 'bestaudio/best',
+            'outtmpl': f'{carpeta}/%(title)s.%(ext)s',
+            'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+            }],
+        }
+        print("Descargando...")
+        with yt_dlp.YoutubeDL(opciones) as ydl:
+            ydl.download([url])
+        print("Descarga finalizada")
+    except Exception as e:
+        print("Error: ",e)
+if __name__ == '__main__':
+    url = input("Ingrese la URL del video: ")
+    descargar_musica(url)
